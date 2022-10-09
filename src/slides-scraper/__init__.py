d11181db663053038ac57933a2d731e7bfb184b1
Sondre Grønås <44143748+sondregronas@users.noreply.github.com>
2022-10-09 13:06:38 +0200

    Create __init__.py

---
+++ src/slides-scraper/__init__.py
@@ -0,0 +1,8 @@
+"""
+A tool to scrape images and other assets from a slides presentation to local storage
+
+Usage:
+    Call with python main.py <path-to-input-file> <path-to-output-file>
+
+    Only partially tested
+"""
